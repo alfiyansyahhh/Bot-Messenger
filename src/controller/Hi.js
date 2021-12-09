456b02a5dac6cab5ab9cc8a17b39d001465dd736
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2021-12-09 21:51:40 +0700

    add

--- src/controller/Hi.js
+++ src/controller/Hi.js
@@ -4,6 +4,7 @@ module.exports = {
     let session = context.session;
     console.log(context.session, 'ini sesion');
     console.log(session._state, 'ini state');
+    console.log(session.lastActivity, 'ini ac');
 
     await context.sendText('Hi, whats your name?');
   },
