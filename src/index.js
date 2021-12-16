04fc53fdef83542260300aad0b5299b397a02560
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2021-12-17 01:49:21 +0700

    improved folder structure/layering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const { router, text } = require('bottender/router');
-const { Hello } = require('./controller/Hi');
-const Action = require('./controller/Action');
+const { router, text, payload } = require('bottender/router');
+const { Hello } = require('./messenger/Hi');
+const Action = require('./messenger/action.messenger');
 
 module.exports = async function App() {
   return router([
+    payload('GET_STARTED', Hello),
     // call Hello action when receiving "hello"/"hi" (case-insensitive) text messages
     text(/^(hello|hi)$/i, Hello),
     // call Main action for any text except "hello/hi"
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,18 @@
 const App = require('.');
+const request = require('supertest');
+const { expect } = require('chai');
 
-describe('index.js', () => {
-  it('should be defined', () => {
-    expect(App).toBeDefined();
+describe('action.js', () => {
+  it('test get message', () => {
+    request(App)
+      .get('/product')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('object');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 });
