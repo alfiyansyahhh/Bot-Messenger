caebe5c1e1e293b0ec13929759c11350c8ec2f6a
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2021-12-21 14:09:14 +0700

    add condition in hi

--- src/messenger/Hi.js
+++ src/messenger/Hi.js
@@ -1,6 +1,35 @@
 module.exports = {
   Hello: async (context) => {
-    // response chat
-    await context.sendText('Hi, whats your name?');
+    var State = context.state;
+    if (State.nama == '') {
+      // response chat
+      await context.sendText('Hi, whats your name?');
+    } else if (State.nama !== '' && State.bday == '') {
+      // response chat
+      await context.sendText(
+        `Hi, ${State.nama} when is your birthday? Please answer in YYYY-MM-DD format.`
+      );
+    } else if (State.nama !== '' && State.bday !== '') {
+      // response chat
+      await context.sendText(
+        `Hi, ${State.nama} Your birthday is at ${State.bday.format(
+          'LL'
+        )}. Do you want to know how many days till your next birthday?`,
+        {
+          quickReplies: [
+            {
+              contentType: 'text',
+              title: 'Yes',
+              payload: 'YES',
+            },
+            {
+              contentType: 'text',
+              title: 'No',
+              payload: 'NO',
+            },
+          ],
+        }
+      );
+    }
   },
 };
